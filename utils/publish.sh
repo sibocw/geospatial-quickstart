3f6e8bd0b36b98f8122fd9d0f774566c9d036e23
Sibo Wang <sibowsb@gmail.com>
2020-05-04 00:59:57 -0500

    fall back to 2019 version

--- utils/publish.sh
+++ utils/publish.sh
@@ -11,16 +11,13 @@ git pull origin master
 # make notebook html
 mkdir -p html
 cd html
-ln -s -f ../../notebooks .
-for ipynb in $(ls ./notebooks/*.ipynb); do
-    jupyter nbconvert $ipynb --to html --output-dir .
-done
-rm notebooks
+ln -s -f ../notebooks/figures .
 cd ..
-mv html ..
+for ipynb in $(ls ./notebooks/*.ipynb); do
+    jupyter nbconvert $ipynb --to html --output-dir html
+done
 
 # make index html
-cd ..
 python utils/make_index.py
 
 # commit changes
